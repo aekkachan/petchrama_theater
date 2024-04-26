7ec6637e17d4c7add179a860b16ac83041ca4781
Ekkachan Wiwattananon <aekkachan@gmail.com>
2024-04-27 01:22:51 +0700

    remove import

--- lib/presentation/page/home_view.dart
+++ lib/presentation/page/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:petchrama_theater/data/model/top_rate.dart' as _topRate;
 import 'package:petchrama_theater/domain/provider.dart';
 import 'package:petchrama_theater/presentation/page/movie_detail.dart';
 import 'package:petchrama_theater/utils/constants/apis.dart';
